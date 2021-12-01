b2057e33dcdbc379548e1d4ae1c29f56efb6d9d4
Hossein Akbari <hosseinakbari506@gmail.com>
2021-12-01 18:23:22 +0330

    Improving Homepage Responsiveness on Mobile

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
 get_header(); ?>
 
 <div class="home-page">
-    <div class="container">
+    <div class="container responsive-pc">
         <div class="row">
             <div class="col-md-8">
                 <div class="top-posts">
-
                     <?php
                         $my_query = new WP_Query( array( 
                             'category_name' => 'top-posts', 
@@ -32,28 +31,28 @@ get_header(); ?>
                 </div>
             </div>
             <div class="col-md-4">
+                <div class="mini-view">
+                    <?php
+                        $my_query = new WP_Query( array( 
+                            'category_name' => 'mini-view', 
+                            'posts_per_page' => 5 
+                        ) ); 
+                        while ($my_query->have_posts()):
+                        $my_query->the_post();
+                        $do_not_duplicate = $post->ID;?>
 
-                <?php
-                    $my_query = new WP_Query( array( 
-                        'category_name' => 'mini-view', 
-                        'posts_per_page' => 5 
-                    ) ); 
-                    while ($my_query->have_posts()):
-                    $my_query->the_post();
-                    $do_not_duplicate = $post->ID;?>
-
-                <a class="posts-box-link" href=<?php the_permalink(); ?>>
-                    <div class="top-posts-box">
-                        <div class="posts-title-homepage">
-                            <h4><?php the_title() ?></h4>
+                    <a class="posts-box-link" href=<?php the_permalink(); ?>>
+                        <div class="top-posts-box">
+                            <div class="posts-title-homepage">
+                                <h4><?php the_title() ?></h4>
+                            </div>
                         </div>
-                    </div>
-                </a><?php endwhile; ?>
+                    </a><?php endwhile; ?>
+                </div>
             </div>
         </div>
 
         <div class="posts">
-
             <?php
                 $my_query = new WP_Query( array( 
                     'category_name' => 'posts', 
@@ -75,6 +74,42 @@ get_header(); ?>
                             <div class="posts-title-homepage">
                                 <h3><?php the_title() ?></h3>
                                 <h5><?php the_time('d F Y'); ?></h5>
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="posts-excerpt">
+                                <?php the_excerpt();?>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div><?php endwhile; ?>
+        </div>
+    </div>
+    
+    <div class="container responsive-mobile">
+        <h4 class="moblie-h4">آخرین اخبار</h4>
+        <div class="posts">
+            <?php
+                $my_query = new WP_Query( array( 
+                    'category_name' => 'posts', 
+                    'posts_per_page' => 9 
+                ) ); 
+                while ($my_query->have_posts()):
+                $my_query->the_post();
+                $do_not_duplicate = $post->ID;?>
+
+            <div class="row">
+                <a class="posts-box-link" href=<?php the_permalink(); ?>>
+                    <div class="posts-box">
+                        <div class="col-md-4">
+                            <div class="posts-title-homepage">
+                                <h2><?php the_title() ?></h2>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="posts-img">
+                                <?php the_post_thumbnail('single-post-thumbnail'); ?>
                             </div>
                         </div>
                         <div class="col-md-5">
